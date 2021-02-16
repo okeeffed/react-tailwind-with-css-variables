d102fbe2f272078a82bf2539e738e63397540fbd
Dennis O'Keeffe <hello@dennisokeeffe.com>
2021-02-16 12:43:18 +1100

    feat: finish example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const ThemeContext = React.createContext();
+
+function ThemeProvider({ children, baseTheme = '' }) {
+  const [theme, setTheme] = React.useState(baseTheme);
+  const value = [theme, setTheme];
+  return (
+    <ThemeContext.Provider value={value}>
+      <div className={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+}
+
+function useTheme() {
+  const context = React.useContext(ThemeContext);
+  if (!context) {
+    throw new Error(`useCount must be rendered within the CountProvider`);
+  }
+  return context;
+}
+
+function KeepsBaseTheme() {
+  return (
+    <div className="theme-base bg-primary p-4">
+      <p className="text-secondary">This maintains them original theme</p>
+    </div>
+  );
+}
+
+function DependsOnParent() {
+  return (
+    <div className="bg-primary p-4">
+      <p className="text-secondary">
+        This depends on the parent having context or not
+      </p>
+    </div>
+  );
+}
+
+function ThemedExample() {
+  const [, setTheme] = useTheme();
+
+  return (
+    <div className={`bg-primary flex items-center justify-center h-screen`}>
+      <button
+        onClick={() => setTheme(undefined)}
+        className="bg-secondary p-4 mr-4 "
+      >
+        Set base theme
+      </button>
+      <button
+        onClick={() => setTheme('theme-dark')}
+        className="bg-secondary p-4 mr-4 "
+      >
+        Set dark theme
+      </button>
+      <button
+        onClick={() => setTheme('theme-alt')}
+        className="bg-secondary p-4"
+      >
+        Set alt theme
+      </button>
+      <div>
+        <KeepsBaseTheme />
+        <DependsOnParent />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DependsOnParent />
+      <ThemeProvider baseTheme="theme-dark">
+        <ThemedExample />
+      </ThemeProvider>
     </div>
   );
 }
